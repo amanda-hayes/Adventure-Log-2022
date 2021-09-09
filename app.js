b1e7fa2447468c24c1d14db0196e9f8de59abe40
Amanda Hayes <hayesmdesign@gmail.com>
2021-09-09 10:10:21 -0700

    abstracted auth routes into new router file

--- app.js
+++ app.js
@@ -2,22 +2,16 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
-const User = require("./models/user");
+const userRouter = require("./routers/userRouter");
 const characterRouter = require("./routers/characterRouter");
 const mongoose = require("mongoose");
 const MONGOURI = process.env.MONGODB_URI;
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcryptjs");
-const middleware = require("./verify-token");
 const cors = require("cors");
 
 app.use(express.json());
 app.use(cors());
 app.use("/characters", characterRouter);
-
-app.get("/jwt-test", middleware.auth, (req, res) => {
-  res.status(200).json(req.user);
-});
+app.use("/users", userRouter);
 
 /****************
  *  MONGOOSE  *
@@ -32,85 +26,6 @@ mongoose.connect(MONGOURI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 mongoose.connection.once("open", () => {
   console.log("connected to mongoose");
-});
-
-/*********************
- *  ROUTE: REGISTER  *
- *********************/
-app.post("/register", async (req, res) => {
-  // register logic
-  try {
-    // get user input
-    const { email, password } = req.body;
-
-    // validate user input
-    if (!(email && password)) {
-      res.status(400).send("All fields are required");
-    }
-
-    // check if user already exists
-    const existingUser = await User.findOne({ email });
-    if (existingUser) {
-      return res
-        .status(409)
-        .send("User with this email already exists. Please login.");
-    }
-
-    // encrypt user PW
-    encryptedPassword = await bcrypt.hash(password, 10);
-
-    // create user
-    const user = await User.create({
-      email,
-      password: encryptedPassword,
-    });
-
-    // create token
-    const token = jwt.sign(
-      {
-        email: user.email,
-      },
-      process.env.TOKEN_SECRET
-    );
-
-    // save user token
-    user.token = token;
-
-    // return new user
-    res.status(201).json({ token, user });
-  } catch (err) {
-    console.log(err);
-  }
-});
-
-/*********************
- *  ROUTE: LOGIN  *
- *********************/
-app.post("/login", async (req, res) => {
-  try {
-    const { email, password } = req.body;
-
-    if (!(email && password)) {
-      res.status(400).send("All fields are required");
-    }
-
-    const user = await User.findOne({ email });
-
-    // encrypt user PW
-    encryptedPassword = await bcrypt.hash(password, 10);
-
-    if (user && (await bcrypt.compare(password, user.password))) {
-      const token = jwt.sign({ email: user.email }, process.env.TOKEN_SECRET);
-
-      user.token = token;
-
-      res.status(200).json(user);
-    }
-
-    res.status(400).send("invalid credentials");
-  } catch (err) {
-    console.log(err);
-  }
 });
 
 app.listen(PORT, () => {
---
+++ routers/userRouter.js
@@ -0,0 +1,83 @@
+const express = require("express");
+const users = express.Router();
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const middleware = require("../verify-token");
+const User = require("../models/user");
+
+users.get("/jwt-test", middleware.auth, (req, res) => {
+  res.status(200).json(req.user);
+});
+
+/*********************
+ *  ROUTE: REGISTER  *
+ *********************/
+users.post("/register", async (req, res) => {
+  // register logic
+  try {
+    // get user input
+    const { email, password } = req.body;
+    // validate user input
+    if (!(email && password)) {
+      res.status(400).send("All fields are required");
+    }
+    // check if user already exists
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res
+        .status(409)
+        .send("User with this email already exists. Please login.");
+    }
+    // encrypt user PW
+    encryptedPassword = await bcrypt.hash(password, 10);
+    // create user
+    const user = await User.create({
+      email,
+      password: encryptedPassword,
+    });
+    // create token
+    const token = jwt.sign(
+      {
+        email: user.email,
+      },
+      process.env.TOKEN_SECRET
+    );
+    // save user token
+    user.token = token;
+    // return new user
+    res.status(201).json(user);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+/*********************
+ *  ROUTE: LOGIN  *
+ *********************/
+users.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!(email && password)) {
+      res.status(400).send("All fields are required");
+    }
+
+    const user = await User.findOne({ email });
+
+    // encrypt user PW
+    encryptedPassword = await bcrypt.hash(password, 10);
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      const token = jwt.sign({ email: user.email }, process.env.TOKEN_SECRET);
+
+      user.token = token;
+
+      res.status(200).json(user);
+    }
+    res.status(400).send("invalid credentials");
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+module.exports = users;
