70f40e725b6267d77c2a55bce8bf698b9381a0cf
Amanda Hayes <hayesmdesign@gmail.com>
2021-09-09 08:39:58 -0700

    added read and update routes

--- routers/characterRouter.js
+++ routers/characterRouter.js
@@ -15,4 +15,32 @@ characters.post("/create", async (req, res) => {
   }
 });
 
+/****************
+ *  ROUTE: GET  *
+ ****************/
+
+characters.get("/dashboard", async (req, res) => {
+  try {
+    const foundCharacters = await characterModel.find({});
+    res.status(200).json(foundCharacters);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+/****************
+ *  ROUTE: PUT  *
+ ****************/
+try {
+  characters.put("/:id", async (req, res) => {
+    const updatedCharacter = await characterModel.findByIdAndUpdate(
+      req.params.id,
+      req.body
+    );
+    res.status(200).json(updatedCharacter);
+  });
+} catch (error) {
+  console.log(error);
+}
+
 module.exports = characters;
