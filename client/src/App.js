9729d04417836e4dda48af3d9990d0011ecb662d
Amanda Hayes <hayesmdesign@gmail.com>
2021-09-17 10:58:25 -0700

    created main pages and added to router

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,19 @@
 import "./App.css";
+import { Switch, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>New Adventure Log</h1>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/Dashboard" exact component={Dashboard} />
+          <Route path="/Login" exact component={Login} />
+        </Switch>
       </header>
     </div>
   );
