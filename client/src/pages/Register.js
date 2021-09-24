079c90f6154e0a875bc68d3c5e4c5b516e7ae422
Amanda Hayes <hayesmdesign@gmail.com>
2021-09-24 09:21:47 -0700

    created registration form, no handle submit logic attached

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,9 +1,68 @@
-import "./App.css";
+import "../App.css";
+import { useReducer, useState } from "react";
+
+const formReducer = (state, event) => {
+  if (event.reset) {
+    return {
+      email: "",
+      password: "",
+    };
+  }
+  return {
+    ...state,
+    [event.name]: event.value,
+  };
+};
 
 const Register = () => {
+  const [formData, setFormData] = useReducer(formReducer, {});
+  const [submitting, setSubmitting] = useState(false);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setSubmitting(true);
+
+    setTimeout(() => {
+      setSubmitting(false);
+      setFormData({
+        reset: true,
+      });
+    }, 3000);
+  };
+
+  const handleChange = (event) => {
+    setFormData({
+      name: event.target.name,
+      value: event.target.value,
+    });
+  };
+
   return (
-    <div>
-      <h1>Register Your Account</h1>
+    <div className="wrapper">
+      <h1>Register</h1>
+      {submitting && <div>Creating Account...</div>}
+      <form onSubmit={handleSubmit}>
+        <fieldset>
+          <label>
+            <p>Email</p>
+            <input
+              name="Email"
+              onChange={handleChange}
+              value={formData.email || ""}
+              placeholder={"email@example.com"}
+            />
+          </label>
+          <label>
+            <p>Password</p>
+            <input
+              name="Password"
+              onChange={handleChange}
+              value={formData.password || ""}
+            />
+          </label>
+        </fieldset>
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 };
