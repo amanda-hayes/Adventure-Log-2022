4d09489e7755470daad1ef9522f850869ef17e16
Amanda Hayes <hayesmdesign@gmail.com>
2021-09-26 20:36:11 -0700

    created formika form and validation

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,70 +1,112 @@
 import "../App.css";
-import { useReducer, useState } from "react";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import * as Yup from "yup";
 
-const formReducer = (state, event) => {
-  if (event.reset) {
-    return {
-      email: "",
-      password: "",
-    };
-  }
-  return {
-    ...state,
-    [event.name]: event.value,
-  };
-};
-
-const Register = () => {
-  const [formData, setFormData] = useReducer(formReducer, {});
-  const [submitting, setSubmitting] = useState(false);
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    setSubmitting(true);
-
-    setTimeout(() => {
-      setSubmitting(false);
-      setFormData({
-        reset: true,
-      });
-    }, 3000);
-  };
-
-  const handleChange = (event) => {
-    setFormData({
-      name: event.target.name,
-      value: event.target.value,
-    });
-  };
-
-  return (
-    <div className="wrapper">
-      <h1>Register</h1>
-      {submitting && <div>Creating Account...</div>}
-      <form onSubmit={handleSubmit}>
-        <fieldset>
-          <label>
-            <p>Email</p>
-            <input
-              name="Email"
-              onChange={handleChange}
-              value={formData.email || ""}
-              placeholder={"email@example.com"}
+const RegisterForm = () => (
+  <>
+    <h1>Register</h1>
+    <Formik
+      initialValues={{
+        email: "",
+        password: "",
+        confirmPassword: "",
+      }}
+      validationSchema={Yup.object().shape({
+        email: Yup.string()
+          .email("Email is invalid")
+          .required("Email is required"),
+        password: Yup.string()
+          .min(6, "Password must be at least 6 characters")
+          .required("Password is required"),
+        confirmPassword: Yup.string()
+          .oneOf([Yup.ref("password"), null], "Passwords must match")
+          .required("Confirm Password is required"),
+      })}
+      onSubmit={async () => {
+        try {
+          const response = await fetch("http://localhost:5000/users/register", {
+            method: "POST",
+            headers: {
+              "Content-type": "application/json",
+            },
+          });
+          const data = await response.json();
+          console.log(data);
+        } catch (error) {
+          console.error(error);
+        }
+      }}
+    >
+      {({ errors, status, touched, isSubmitting }) => (
+        <Form>
+          <div className="form-group">
+            <label htmlFor="email">Email</label>
+            <Field
+              name="email"
+              type="text"
+              className={
+                "form-control" +
+                (errors.email && touched.email ? " is-invalid" : "")
+              }
             />
-          </label>
-          <label>
-            <p>Password</p>
-            <input
-              name="Password"
-              onChange={handleChange}
-              value={formData.password || ""}
+            <ErrorMessage
+              name="email"
+              component="div"
+              className="invalid-feedback"
             />
-          </label>
-        </fieldset>
-        <button type="submit">Submit</button>
-      </form>
-    </div>
-  );
-};
+          </div>
+          <div className="form-row">
+            <div className="form-group col">
+              <label htmlFor="password">Password</label>
+              <Field
+                name="password"
+                type="password"
+                className={
+                  "form-control" +
+                  (errors.password && touched.password ? " is-invalid" : "")
+                }
+              />
+              <ErrorMessage
+                name="password"
+                component="div"
+                className="invalid-feedback"
+              />
+            </div>
+            <div className="form-group col">
+              <label htmlFor="confirmPassword">Confirm Password</label>
+              <Field
+                name="confirmPassword"
+                type="password"
+                className={
+                  "form-control" +
+                  (errors.confirmPassword && touched.confirmPassword
+                    ? " is-invalid"
+                    : "")
+                }
+              />
+              <ErrorMessage
+                name="confirmPassword"
+                component="div"
+                className="invalid-feedback"
+              />
+            </div>
+          </div>
+          <div className="form-group">
+            <button
+              type="submit"
+              className="btn btn-primary mr-2"
+              disabled={isSubmitting}
+            >
+              Register
+            </button>
+            <button type="reset" className="btn btn-secondary">
+              Reset
+            </button>
+          </div>
+        </Form>
+      )}
+    </Formik>
+  </>
+);
 
-export default Register;
+export default RegisterForm;
