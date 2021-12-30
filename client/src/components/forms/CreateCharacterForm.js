e2c0d176b66d69efc90a38a576881b20892d28a8
Amanda Hayes <hayesmdesign@gmail.com>
2021-12-30 10:22:03 -0800

    wrote form

--- client/src/components/forms/CreateCharacterForm.js
+++ client/src/components/forms/CreateCharacterForm.js
@@ -2,7 +2,7 @@ import "../App.css";
 import { Formik, Field, Form, ErrorMessage } from "formik";
 import * as Yup from "yup";
 
-const CreateCharacterForm = () => {
+const CreateCharacterForm = (props) => {
   <Formik
     initialValues={{
       name: "",
@@ -25,14 +25,54 @@ const CreateCharacterForm = () => {
     }}
     onSubmit={async (values) => {
       try {
-        const response = await fetch("http://localhost:5000/characters/create", {
-          method:
-        }
-        )
+        const response = await fetch(
+          "http://localhost:5000/characters/create",
+          {
+            method: "POST",
+            headers: {
+              "Content-type": "application/json",
+            },
+            body: JSON.stringify(values),
+          }
+        );
+        const data = await response.json();
+        console.log(data);
+        alert("Character created");
+        props.history.push("/Dashboard");
+      } catch (error) {
+        console.log(error);
+        alert("Error");
       }
     }}
-  ></Formik>;
-  return <h1>Create new Character</h1>;
+  >
+    {({ errors, touched, isSubmitting }) => (
+      <Form>
+        <div className="form-group">
+          <label htmlFor="name">Name</label>
+          <Field
+            name="name"
+            type="text"
+            className={
+              "form-control" +
+              (errors.name && touched.name ? " is-invalid" : "")
+            }
+          />
+          <div className="form-group">
+            <button
+              type="submit"
+              className="btn btn-primary mr-2"
+              disabled={isSubmitting}
+            >
+              Create
+            </button>
+            {/* <button type="reset" className="btn btn-secondary">
+              Reset
+            </button> */}
+          </div>
+        </div>
+      </Form>
+    )}
+  </Formik>;
 };
 
 export default CreateCharacterForm;
