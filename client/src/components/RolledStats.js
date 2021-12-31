1a43322fa48cfd9e0b70edcf13822b0fe572e7fe
Amanda Hayes <hayesmdesign@gmail.com>
2021-12-30 22:34:25 -0800

    added some fields for stats

--- client/src/components/RolledStats.js
+++ client/src/components/RolledStats.js
@@ -10,6 +10,23 @@ const RolledStats = () => (
     <Field type="text" name="ac" className="formRow"></Field>
     <label htmlFor="weapon">Weapon</label>
     <Field type="text" name="weapon" className="formRow"></Field>
+    <label htmlFor="strength">Strength</label>
+    <Field type="text" name="strength"></Field>
+    <label htmlFor="dexterity">Dexterity</label>
+    <Field type="text" name="dexterity"></Field>
+    <label htmlFor="constitution">Constitution</label>
+    <Field type="text" name="constitution"></Field>
+    <label htmlFor="intelligence">Intelligence</label>
+    <Field type="text" name="intelligence"></Field>
+    <label htmlFor="wisdom">Wisdom</label>
+    <Field type="text" name="wisdom"></Field>
+    <label htmlFor="charisma">Charisma</label>
+    <Field type="text" name="charisma"></Field>
+    <label htmlFor="hp">Hit Points</label>
+    <Field type="text" name="hp"></Field>
+    <button type="submit" className="btn btn-primary mr-2">
+      Roll Stats
+    </button>
   </>
 );
 
