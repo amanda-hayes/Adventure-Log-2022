c2ad607ab02712b5ec28524b25a72b338b11def3
Amanda Hayes <hayesmdesign@gmail.com>
2022-07-21 14:29:24 -0700

    added damage to weapon model

--- models/weaponModel.js
+++ models/weaponModel.js
@@ -3,6 +3,7 @@ const { Schema, model } = require("mongoose");
 const weaponModel = Schema({
   Index: { type: Number, required: true },
   Name: { type: String, required: true },
+  Damage: { type: Number, required: true },
 });
 
 module.exports = model("Weapon", weaponModel);
